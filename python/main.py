366c873d3c38c49284531564f58983ed3e2294c0
ashira09 <vecnivtil89@gmail.com>
2026-05-08 15:20:52 +0800

    Базовое API для обработки входящих сообщения из bitrix

---
+++ python/main.py
@@ -0,0 +1,15 @@
+# main.py\
+import logging
+from fastapi import FastAPI, Request
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+app = FastAPI()
+
+app = FastAPI()
+
+@app.post("/bitrix-event")
+async def echo(request: Request):
+    body = await request.body()
+    logger.info(f"📦 RAW BODY: {body.decode()[:500]}")
+    return {"status": "ok", "received": True}
