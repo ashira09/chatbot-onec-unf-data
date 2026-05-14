c7e956e5a85e650f1337c726875d970d1408f9ed
ashira09 <vecnivtil89@gmail.com>
2026-05-14 16:17:09 +0800

    Исключены необязательные .env переменные из проверки

--- src/bitrix/load.py
+++ src/bitrix/load.py
@@ -13,5 +13,5 @@ BOT_ID=getenv('BOT_ID')
 BOT_EVENT_MODE=getenv('BOT_EVENT_MODE')
 BOT_HANDLER_URL=getenv('BOT_HANDLER_URL')
 
-if any([var in [None, ''] for var in [WEBHOOK, BOT_TOKEN, BOT_NAME, BOT_CODE, BOT_WORK_POSITION, BOT_ID, BOT_EVENT_MODE, BOT_HANDLER_URL]]):
+if any([var in [None, ''] for var in [WEBHOOK, BOT_TOKEN, BOT_NAME, BOT_CODE, BOT_WORK_POSITION, BOT_EVENT_MODE]]):
     raise Exception('Заданы не все переменные в .env!')
--- src/onec/load.py
+++ src/onec/load.py
@@ -8,5 +8,5 @@ ONEC_HOST=getenv('ONEC_HOST')
 ONEC_CONF_USER=getenv('ONEC_CONF_USER')
 ONEC_CONF_PASSWORD=getenv('ONEC_CONF_PASSWORD')
 
-if any([var in [None, ''] for var in [ONEC_HOST, ONEC_CONF_USER, ONEC_CONF_PASSWORD]]):
+if any([var in [None, ''] for var in [ONEC_HOST, ONEC_CONF_USER]]):
     raise Exception('Заданы не все переменные в .env!')
