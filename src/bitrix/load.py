38e9b3496bd376f0e926f431595828b4b6346c21
ashira09 <vecnivtil89@gmail.com>
2026-05-15 16:26:27 +0800

    Удаление уимпорта убранных переменных .env

--- src/bitrix/load.py
+++ src/bitrix/load.py
@@ -10,8 +10,6 @@ BOT_NAME=getenv('BOT_NAME')
 BOT_CODE=getenv('BOT_CODE')
 BOT_WORK_POSITION=getenv('BOT_WORK_POSITION')
 BOT_ID=getenv('BOT_ID')
-BOT_EVENT_MODE=getenv('BOT_EVENT_MODE')
-BOT_HANDLER_URL=getenv('BOT_HANDLER_URL')
 
-if any([var in [None, ''] for var in [WEBHOOK, BOT_TOKEN, BOT_NAME, BOT_CODE, BOT_WORK_POSITION, BOT_EVENT_MODE]]):
+if any([var in [None, ''] for var in [WEBHOOK, BOT_TOKEN, BOT_NAME, BOT_CODE, BOT_WORK_POSITION]]):
     raise Exception('Заданы не все переменные в .env!')
