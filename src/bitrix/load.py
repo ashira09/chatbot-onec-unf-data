381fd28f27f109036694b1fa05d85e4091e01db2
ashira09 <vecnivtil89@gmail.com>
2026-05-16 13:35:26 +0800

    Изменил загрузку переменных .env в связи с изменениями

--- src/bitrix/load.py
+++ src/bitrix/load.py
@@ -9,7 +9,6 @@ BOT_TOKEN=getenv('BOT_TOKEN')
 BOT_NAME=getenv('BOT_NAME')
 BOT_CODE=getenv('BOT_CODE')
 BOT_WORK_POSITION=getenv('BOT_WORK_POSITION')
-BOT_ID=getenv('BOT_ID')
 
 if any([var in [None, ''] for var in [WEBHOOK, BOT_TOKEN, BOT_NAME, BOT_CODE, BOT_WORK_POSITION]]):
     raise Exception('Заданы не все переменные в .env!')
--- src/yandex_cloud/load.py
+++ src/yandex_cloud/load.py
@@ -7,11 +7,11 @@ load_dotenv(dotenv_file)
 OAUTH_TOKEN=getenv('OAUTH_TOKEN')
 IAM_TOKEN=getenv('IAM_TOKEN')
 FOLDER_ID=getenv('FOLDER_ID')
-FILE_ID=getenv('FILE_ID')
-VECTOR_STORE_ID=getenv('VECTOR_STORE_ID')
+FILE_TOKEN=getenv('FILE_TOKEN')
+VECTOR_STORE_TOKEN=getenv('VECTOR_STORE_TOKEN')
 MODEL=getenv('MODEL')
 PATH_TO_CHUNKS=getenv('PATH_TO_CHUNKS')
 BASE_URL=getenv('BASE_URL')
 
-if any([var in [None, ''] for var in [OAUTH_TOKEN, FOLDER_ID, MODEL, PATH_TO_CHUNKS, BASE_URL]]):
+if any([var in [None, ''] for var in [OAUTH_TOKEN, FOLDER_ID, FILE_TOKEN, VECTOR_STORE_TOKEN, MODEL, PATH_TO_CHUNKS, BASE_URL]]):
     raise Exception('Заданы не все переменные в .env!')
