5de370465222a34c6353eb6652281a07712ebe70
ashira09 <vecnivtil89@gmail.com>
2026-05-24 22:33:23 +0800

    Поправил загрузку DATABASE_URL

--- src/database/database.py
+++ src/database/database.py
@@ -1,13 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from src.database.models import Base
-import os
+from os import getenv
+from dotenv import find_dotenv, load_dotenv
+
+dotenv_file = find_dotenv()
+load_dotenv(dotenv_file)
 
 # Получаем данные для подключения из переменных окружения
-DATABASE_URL = os.getenv(
-    "DATABASE_URL", 
-    "postgresql://postgres:postgres@localhost:5432/chatbot_db"
-)
+DATABASE_URL = getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
